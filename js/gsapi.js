88cdcb1d89ff9de7fb8560afdaca2130e7980fc2
aurelique <aurelique.perfumery@gmail.com>
2025-08-29 16:34:25 +0000

    gsapi

--- js/gsapi.js
+++ js/gsapi.js
@@ -1,25 +1,56 @@
+// URL Web App (hasil deploy Apps Script)
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz4_80DlESn-zfB6S5W8BLy2DG1Pqo-20evsNgzxCYd5Od6R3sgeZy1UFMCT-p3lEI/exec";
 
-// Generic GET
-async function apiGet(action){
-  const res = await fetch(`${SCRIPT_URL}?action=${action}`);
+/**
+ * Generic GET
+ */
+async function apiGet(action) {
+  const url = `${SCRIPT_URL}?action=${action}`;
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("API GET error " + res.status);
   return res.json();
 }
 
-// Generic POST
+/**
+ * Generic POST
+ */
 async function apiPost(action, data = {}) {
   data.action = action;
   const res = await fetch(SCRIPT_URL, {
     method: "POST",
     body: JSON.stringify(data),
-    headers: {"Content-Type": "application/json"}
+    headers: { "Content-Type": "application/json" }
   });
+  if (!res.ok) throw new Error("API POST error " + res.status);
   return res.json();
 }
 
-// Exports
-window.getProducts = ()=> apiGet("getProducts");
-window.getOrders = ()=> apiGet("getOrders");
-window.getSettings = ()=> apiGet("getSettings");
-window.addOrder = (d)=> apiPost("addOrder", d);
-window.updateOrderStatus = (id,status)=> apiPost("updateOrderStatus",{orderId:id,status});
+// ============================
+// Endpoints
+// ============================
+async function getProducts() {
+  return await apiGet("getProducts");
+}
+
+async function getOrders() {
+  return await apiGet("getOrders");
+}
+
+async function addOrder(orderData) {
+  return await apiPost("addOrder", orderData);
+}
+
+async function uploadProof(orderId, proofBase64) {
+  return await apiPost("uploadProof", { orderId, proof: proofBase64 });
+}
+
+async function updateOrderStatus(orderId, status) {
+  return await apiPost("updateOrderStatus", { orderId, status });
+}
+
+// expose ke global
+window.getProducts = getProducts;
+window.getOrders = getOrders;
+window.addOrder = addOrder;
+window.uploadProof = uploadProof;
+window.updateOrderStatus = updateOrderStatus;
